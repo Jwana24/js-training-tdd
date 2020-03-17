de801cfcd6eeb2b17d3ee74559e593f9012c9e84
Jwana24 <detrieux.btscom13@gmail.com>
2020-03-17 16:44:52 +0100

    update total test, OK

--- exercises/total.js
+++ exercises/total.js
@@ -7,6 +7,14 @@
  */
 
 // Your code:
+const myArrayNumber = [];
+function total(myArrayNumber)
+{
+    
+}
+return total(myArrayNumber.length);
+
+
 
 //* Begin of tests
 const assert = require('assert');
