1fce74a774f7ebb96bce8eb25505970ce6d9b0e3
Jwana24 <detrieux.btscom13@gmail.com>
2020-03-20 11:36:14 +0100

    test yell.js OK

--- exercises/yell.js
+++ exercises/yell.js
@@ -7,9 +7,18 @@
  */
 
 // Your code:
+function yell(string)
+{
+    return string.toUpperCase();
+}
+
+
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+// assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.strictEqual(yell.length, 1);
+assert.strictEqual(yell("je m'apelle johanna"), "JE M'APELLE JOHANNA");
 // End of tests */
