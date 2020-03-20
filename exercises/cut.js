88f8ab19428280f866fcd21714bff515970bdeea
Jwana24 <detrieux.btscom13@gmail.com>
2020-03-20 12:01:34 +0100

    test cut.js OK

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,42 @@
  */
 
 // Your code:
+function cutFirst(string)
+{
+    return string.substring(1);
+}
+
+function cutLast(string)
+{
+    return string.substring(0, string.length - 1);
+}
+
+function cutFirstLast(string)
+{
+    return string.substring(1, string.length - 1);
+}
+
+
 
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+// assert.fail('You must write your own tests');
+
+// Tests for the cutFirst function
+assert.strictEqual(typeof cutFirst, "function");
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutFirst("enveloppe"), "nveloppe");
+
+// Tests for the cutLast function
+assert.strictEqual(typeof cutLast, "function");
+assert.strictEqual(cutLast.length, 1);
+assert.strictEqual(cutLast("fauteuil"), "fauteui");
+
+// Tests for the cutFirstLast function
+assert.strictEqual(typeof cutFirstLast, "function");
+assert.strictEqual(cutFirstLast.length, 1);
+assert.strictEqual(cutFirstLast("livres"), "ivre");
+
+
 // End of tests */
